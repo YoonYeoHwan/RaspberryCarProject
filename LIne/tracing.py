bdd67d439dc3af933f7ebec908783e98ea523b2a
YeoHwan Yoon <johnyhgmg@kookmin.ac.kr>
2017-11-13 10:57:44 +0900

    a

---
+++ LIne/tracing.py
@@ -0,0 +1,171 @@
+#########################################################################
+### Date: 2017/10/13
+### file name: trackingModule.py
+### Purpose: this code has been generated for the five-way tracking sensor
+###         to perform the decision of direction
+###
+#########################################################################
+
+# =======================================================================
+# import GPIO library and time module
+# =======================================================================
+import RPi.GPIO as GPIO
+from time import sleep
+from ultraModule import getDistance
+
+# =======================================================================
+# import TurnModule() method
+# =======================================================================
+from TurnModule import *
+
+
+# =======================================================================
+# rightPointTurn() and leftPointTurn() in TurnModule module
+# =======================================================================
+# student assignment (1)
+# student assignment (2)
+
+
+
+# =======================================================================
+# import go_forward_any(), go_backward_any(), stop(), LeftPwm(),
+# RightPwm(), pwm_setup(), and pwm_low() methods in the module of go_any
+# =======================================================================
+from go_any import *
+# =======================================================================
+#  set GPIO warnings as false
+# =======================================================================
+GPIO.setwarnings(False)
+
+# =======================================================================
+# set up GPIO mode as BOARD
+# =======================================================================
+GPIO.setmode(GPIO.BOARD)
+
+# =======================================================================
+# declare the pins of 16, 18, 22, 40, 32 in the Rapberry Pi
+# as the control pins of 5-way trackinmg sensor in order to
+# control direction
+#
+#  leftmostled    leftlessled     centerled     rightlessled     rightmostled
+#       16            18              22             40              32
+#
+# led turns on (1) : trackinmg sensor led detects white playground
+# led turns off(0) : trackinmg sensor led detects black line
+
+# leftmostled off : it means that moving object finds black line
+#                   at the position of leftmostled
+#                   black line locates below the leftmostled of the moving object
+#
+# leftlessled off : it means that moving object finds black line
+#                   at the position of leftlessled
+#                   black line locates below the leftlessled of the moving object
+#
+# centerled off : it means that moving object finds black line
+#                   at the position of centerled
+#                   black line locates below the centerled of the moving object
+#
+# rightlessled off : it means that moving object finds black line
+#                   at the position of rightlessled
+#                   black line locates below the rightlessled  of the moving object
+#
+# rightmostled off : it means that moving object finds black line
+#                   at the position of rightmostled
+#                   black line locates below the rightmostled of the moving object
+# =======================================================================
+
+leftmostled = 16
+leftlessled = 18
+centerled = 22
+rightlessled = 40
+rightmostled = 32
+
+# =======================================================================
+# because the connetions between 5-way tracking sensor and Rapberry Pi has been
+# established, the GPIO pins of Rapberry Pi
+# such as leftmostled, leftlessled, centerled, rightlessled, and rightmostled
+# should be clearly declared whether their roles of pins
+# are output pin or input pin
+# since the 5-way tracking sensor data has been detected and
+# used as the input data, leftmostled, leftlessled, centerled, rightlessled, and rightmostled
+# should be clearly declared as input
+#
+# =======================================================================
+
+GPIO.setup(leftmostled, GPIO.IN)
+GPIO.setup(leftlessled, GPIO.IN)
+GPIO.setup(centerled, GPIO.IN)
+GPIO.setup(rightlessled, GPIO.IN)
+GPIO.setup(rightmostled, GPIO.IN)
+
+# =======================================================================
+# GPIO.input(leftmostled) method gives the data obtained from leftmostled
+# leftmostled returns (1) : leftmostled detects white playground
+# leftmostled returns (0) : leftmostled detects black line
+#
+#
+# GPIO.input(leftlessled) method gives the data obtained from leftlessled
+# leftlessled returns (1) : leftlessled detects white playground
+# leftlessled returns (0) : leftlessled detects black line
+#
+# GPIO.input(centerled) method gives the data obtained from centerled
+# centerled returns (1) : centerled detects white playground
+# centerled returns (0) : centerled detects black line
+#
+# GPIO.input(rightlessled) method gives the data obtained from rightlessled
+# rightlessled returns (1) : rightlessled detects white playground
+# rightlessled returns (0) : rightlessled detects black line
+#
+# GPIO.input(rightmostled) method gives the data obtained from rightmostled
+# rightmostled returns (1) : rightmostled detects white playground
+# rightmostled returns (0) : rightmostled detects black line
+#
+# =======================================================================
+
+
+def signal(n1, n2, n3, n4, n5):
+    return A == n1 and B == n2 and C == n3 and D == n4 and E == n5
+
+
+
+list = [80, 0.5]
+try:
+    while True:
+        A = GPIO.input(leftmostled)
+        B = GPIO.input(leftlessled)
+        C = GPIO.input(centerled)
+        D = GPIO.input(rightlessled)
+        E = GPIO.input(rightmostled)
+
+
+
+        if signal(1, 1, 0, 1, 1):
+            go_forward_any(1, 1)
+
+        elif signal(1, 0, 0, 1, 1):
+            go_forward_any(1, 1)
+
+        elif signal(1, 1, 0, 0, 1):
+            go_forward_any(1, 1)
+
+        elif signal(0, 0, 1, 1, 1):
+            lpt(3)
+
+        elif signal(0, 1, 1, 1, 1):
+            lpt(6)
+
+        elif signal(1, 1, 1, 0, 0):
+            rpt(3)
+
+        elif signal(1, 1, 1, 0, 0):
+            rpt(6)
+
+        elif signal(0, 0, 0, 0, 0):
+            stop()
+
+
+
+
+
+except KeyboardInterrupt:
+    GPIO.cleanup()
