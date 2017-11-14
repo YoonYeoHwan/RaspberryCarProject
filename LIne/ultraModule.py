111561ca86b169d2daec90d637d8f20ae756fc10
YoonYeoHwan <johnyhgmg@kookmin.ac.kr>
2017-11-14 18:18:22 +0900

    Update ultraModule.py

--- LIne/ultraModule.py
+++ LIne/ultraModule.py
@@ -13,24 +13,31 @@ import time
 
 GPIO.setmode(GPIO.BOARD)
 
-trig=33
-echo=31
+trig = 33
+echo = 31
 
 #ultrasonic sensor setting
-GPIO.setup(trig,GPIO.OUT)
-GPIO.setup(echo,GPIO.IN)
+GPIO.setup(trig, GPIO.OUT)
+GPIO.setup(echo, GPIO.IN)
+
+pulse_end = 0
+pulse_start = 0
 
 def getDistance():
-    GPIO.output(trig,False)
-    time.sleep(0.5)
-    GPIO.output(trig,True)
+    global pulse_end
+    global pulse_start
+    
+    GPIO.output(trig, False)
     time.sleep(0.00001)
-    GPIO.output(trig,False)
-    while GPIO.input(echo)==0:
-        pulse_start=time.time()
-    while GPIO.input(echo)==1:
-        pulse_end=time.time()
-    pulse_duration=pulse_end-pulse_start
-    distance=pulse_duration*17000
-    distance=round(distance,2)
+    GPIO.output(trig, True)
+    time.sleep(0.00001)
+    GPIO.output(trig, False)
+    
+    while GPIO.input(echo) == 0:
+        pulse_start = time.time()
+    while GPIO.input(echo) == 1:
+        pulse_end = time.time()
+    pulse_duration = pulse_end - pulse_start
+    distance = pulse_duration * 17000
+    distance = round(distance, 2)
     return distance
